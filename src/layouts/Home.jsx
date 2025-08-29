fd8319215e726010c5daa5a45858622ae2f4f8c0
Agustin Invaldi <agustin.invaldi@emprinet.com>
2025-08-28 21:00:18 -0300

    add-localstorage-and-react-router-dom

---
+++ src/layouts/Home.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react"
+import { useNavigate } from "react-router-dom"
+import { Button } from "primereact/button"
+
+const Home = () =>{
+
+    const navigate = useNavigate()
+    return(
+        <Fragment>
+            <h2>Mi Home</h2>
+            <Button onClick={() => navigate('/tarjeta')} label='Ir al formulario' ></Button>
+        </Fragment>
+    )
+}
+
+export default Home
